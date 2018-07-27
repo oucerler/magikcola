1586b5c3698fd7cf0d0f6c9f0d90338750650d08
Omer Ucerler <oucerler@gmail.com>
2018-07-27 15:55:00 +0200

    tweede commit

--- magikcola/src/main/java/com/qien/magikcola/api/PepsiEndpoint.java
+++ magikcola/src/main/java/com/qien/magikcola/api/PepsiEndpoint.java
@@ -1,5 +1,31 @@
 package com.qien.magikcola.api;
 
-public class PepsiEndpoint {
+	import javax.ws.rs.Consumes;
+	import javax.ws.rs.GET;
+	import javax.ws.rs.POST;
+	import javax.ws.rs.Path;
+	import javax.ws.rs.Produces;
+	import javax.ws.rs.core.MediaType;
+	import javax.ws.rs.core.Response;
 
-}
+	import org.springframework.beans.factory.annotation.Autowired;
+	import org.springframework.stereotype.Component;
+
+	import com.qien.magikcola.domein.Pepsi;
+	import com.qien.magikcola.controller.PepsiService;
+
+	@Path("pepsi")
+	@Component
+	public class PepsiEndpoint {
+		
+		@Autowired
+		private PepsiService pepsiService;
+
+		@GET
+		@Produces(MediaType.APPLICATION_JSON)
+		public Response listGroep(){
+			Iterable <Pepsi> pepsi = pepsiService.maaktNietUit();
+			return Response.ok(pepsi).build();
+		}
+		
+	}
